a6d99b84a4fcbc239509135ff079c9d36919b047
Victor Couty <victor@couty.eu>
2016-09-28 22:55:09 +0200

    Initial commit

---
+++ client/glob.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#coding: utf-8
+"""
+Program by Victor Couty (victor@couty.eu)
+2016
+"""
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION = str(VERSION_MAJOR)+"."+str(VERSION_MINOR)
---
+++ server/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#coding: utf-8
+"""
+Program by Victor Couty (victor@couty.eu)
+2016
+"""
+
+from glob import *
+import sys
+
+if sys.version_info.major != 3:
+  print("Please use Python3 to run this program")
+  sys.exit(-1)
+
+print("Starting FatChat server version {}...".format(VERSION))
+print("FatChat server terminating, bye!")
