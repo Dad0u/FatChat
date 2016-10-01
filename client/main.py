c735cd600a0a6af5556fcd77c2bd2e8b26392939
Victor Couty <victor@couty.eu>
2016-10-01 23:45:32 +0200

    Added Python3 check before imports

--- client/main.py
+++ client/main.py
@@ -7,12 +7,12 @@ Program by Victor Couty (victor@couty.eu)
 
 import sys
 
-from glob import *
-from interface import Main_window
-
 if sys.version_info.major != 3:
   print("Please use Python3 to run this program")
   sys.exit(-1)
+
+from glob import *
+from interface import Main_window
 
 mw = Main_window()
 mw.run()
