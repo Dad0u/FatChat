665f190fb4017e00ce4d5e99e00d387873f62378
Victor Couty <victor@couty.eu>
2016-10-23 23:05:26 +0200

    WIP: Started working on server

---
+++ server/client.py
@@ -0,0 +1,36 @@
+﻿#!/usr/bin/python3
+#coding: utf-8
+"""
+Program by Victor Couty (victor@couty.eu)
+2016
+"""
+
+from glob import *
+import sys
+
+if sys.version_info.major != 3:
+  print("Please use Python3 to run this program")
+  sys.exit(-1)
+
+import select
+
+class Client():
+  def __init__(self, conn, addr):
+    self.ip = addr[0]
+    self.conn = conn
+    self.port = addr[1]
+    self.nick = ""
+
+  def isTalking(self):
+    try:
+      return len(select.select([self.conn],[],[],0)[0]) > 0
+    except:
+      self.disconnect()
+
+  def disconnect(self):
+    print("Deconnexion de "+self.nick)
+    try:
+      self.conn.send(b'EXT')
+    except:
+      pass
+    self.conn.close()
--- server/main.py
+++ server/main.py
@@ -12,5 +12,33 @@ if sys.version_info.major != 3:
   print("Please use Python3 to run this program")
   sys.exit(-1)
 
+import socket
+import select
+
+from client import Client
+
 print("Starting FatChat server version {}...".format(VERSION))
+
+conn = socket.socket()
+conn.bind(('',DEFAULT_PORT))
+conn.listen(3)
+client = []
+
+def auth(c):
+  msg = c.conn.recv(SIZE)
+  if msg == b'YOLO':
+    c.conn.send(b'POUET')
+  else:
+    print("Incorrect auth:",msg)
+    c.conn.send(b'NOPE')
+    client.remove(c)
+
+while True:
+  new_conn = select.select([conn],[],[],0.05)[0]
+  for c in new_conn:
+    client.append(Client(*c.accept()))
+    print("New client connected from "+client[-1].ip+", authentication...")
+    auth(client[-1])
+
+  
 print("FatChat server terminating, bye!")
